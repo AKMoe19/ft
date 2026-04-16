54e65dc336a44868248bfaf2e7cfe80681739fdc
AK M0E <moewe227@gmail.com>
2026-04-16 22:51:46 +0630

    Update server.js
    .env update

--- server.js
+++ server.js
@@ -208,4 +208,8 @@ app.get('/profile', async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+// app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
