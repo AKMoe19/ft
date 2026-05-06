14c117308801a44e6f7a2d64fded7ca74ed30200
AK M0E <moewe227@gmail.com>
2026-05-06 14:45:45 +0630

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    userId: { type: String, unique: true },
+    createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
